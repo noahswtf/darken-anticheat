e11b536320bd8c855168755550a052e51fb9fd41
noahswtf <131519316+noahswtf@users.noreply.github.com>
2023-11-19 21:01:49 +0000

    set up base of project settings

---
+++ protected-process/src/main.cpp
@@ -0,0 +1,8 @@
+// darken anticheat protected process
+// here is what the anticheat attempts to 'protect'
+// in a production scenario, this would be replaced by the application
+
+int main()
+{
+	return 0;
+}
---
+++ user/src/main.cpp
@@ -0,0 +1,8 @@
+// darken anticheat user mode
+// this is the user mode process of the anticheat
+// thisll communicate with the driver to protect the application
+
+int main()
+{
+	return 0;
+}
