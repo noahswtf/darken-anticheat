ca32e7507ae138756e3f7973de807eb115eb6edc
noahswtf <131519316+noahswtf@users.noreply.github.com>
2023-12-03 11:58:03 +0000

    set up entry points for driver and protected process

---
+++ kernel/main.cpp
@@ -0,0 +1,10 @@
+#include <ntifs.h>
+#include <ntddk.h>
+
+NTSTATUS driver_entry(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path)
+{
+	UNREFERENCED_PARAMETER(driver_object);
+	UNREFERENCED_PARAMETER(registry_path);
+
+	return STATUS_SUCCESS;
+}
--- protected-process/src/main.cpp
+++ protected-process/src/main.cpp
@@ -1,8 +1,12 @@
 // darken anticheat protected process
 // here is what the anticheat attempts to 'protect'
 // in a production scenario, this would be replaced by the application
+#include <iostream>
 
 int main()
 {
+	std::cout << "  [darken-anticheat] - started protected process";
+	system("pause");
+
 	return 0;
 }
